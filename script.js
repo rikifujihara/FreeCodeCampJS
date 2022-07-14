98ff2a02fc855222ff8c806699056eae306550cc
rikifujihara <rikifujihara@gmail.com>
2022-07-14 15:09:19 +1000

    card counter

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
-let number = 5; // in-line comment
+// card counting
 
-/* this is a multiline comment
-here is comment as well
-*/
+var count = 0
 
-let number = 10;
+function cc(card) {
+
+    switch (card) {
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+            count++
+            break
+        case 10:
+        case "J":
+        case "Q":
+        case "K":
+        case "A":
+            count--
+            break   
+    }
+
+    let holdbet = (count > 0 ? 'Bet' : 'Hold')
+
+    return count + " " + holdbet
+
+}
+
+cc(2); cc(3); cc(7); cc("K"); cc("A");
+console.log(cc("K"))
